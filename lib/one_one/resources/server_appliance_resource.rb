96b0e052772efead1b2e8bc44c90c6f80eeabcaa
Carlos Sanchez <carlos@bitrock.com>
2016-01-13 17:45:02 +0100

    Add get for server appliances

--- lib/one_one/resources/server_appliance_resource.rb
+++ lib/one_one/resources/server_appliance_resource.rb
@@ -8,6 +8,12 @@ module OneOne
           ServerApplianceMapping.extract_collection(response.body, :read)
         end
       end
+
+      action :find, 'GET /v1/server_appliances/:id' do
+        handler(200) do |response|
+          ServerApplianceMapping.extract_single(response.body, :read)
+        end
+      end
     end
   end
 end
